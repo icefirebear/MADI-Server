ddb7825f926cdaa0e6ba5f1471277b5c3e673574
hanbin8269 <hanbin8269@gmail.com>
2021-01-05 23:39:58 +0900

    Feat : create login test

--- src/tests/api/test_users.py
+++ src/tests/api/test_users.py
@@ -22,3 +22,15 @@ def test_create_user(client: TestClient) -> None:
     response = client.post(f"{BASE_URL}/user/register", json=data)
 
     assert 201 == response.status_code
+
+
+def test_login_user(client: TestClient) -> None:
+    data = {
+        "email": MOCK_USERS["correct_user"]["email"],
+        "password": MOCK_USERS["correct_user"]["password"],
+    }
+    response = client.post(f"{BASE_URL}/user/register", json=data)
+
+    assert 200 == response.status_code
+    token_regular = re.compile("\B[.]\B[.]\B")
+    assert token_regular.match(response.json()["token"])
