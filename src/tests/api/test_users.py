fc5da3a0807b834164cee53f6648fc31cfd2cca8
hanbin8269 <hanbin8269@gmail.com>
2021-01-05 23:39:44 +0900

    Feat : create test user

--- src/tests/api/test_users.py
+++ src/tests/api/test_users.py
@@ -1,9 +1,24 @@
+import re
+
 from fastapi.testclient import TestClient
 from sqlalchemy.orm import Session
 
-def test_create_user(
-    client: TestClient
-):
-    a =  1
-    print( a  )
 
+BASE_URL = "http://localhost:8000"
+
+MOCK_USERS = {
+    "correct_user": {
+        "email": "student1@example.com",
+        "password": "1234",
+        "name": "학생1",
+        "stdNo": 2217,
+        "sex": "male",
+    }
+}
+
+
+def test_create_user(client: TestClient) -> None:
+    data = MOCK_USERS["correct_user"]
+    response = client.post(f"{BASE_URL}/user/register", json=data)
+
+    assert 201 == response.status_code
