2033bf2bb9b72a17336771ba937b1c663333cd07
icefirebear <woungsub1234@gmail.com>
2021-01-05 23:04:44 +0900

    feat: login endpoint 추가

--- src/app/api/api/endpoints/login.py
+++ src/app/api/api/endpoints/login.py
@@ -1,5 +1,17 @@
+from typing import Any
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 router = APIRouter()
 
+
+# 기본정보로 로그인
+@router.post("/login")
+def login_general() -> Any:
+    return
+
+
+# access-token 으로 로그인
+@router.post("/login/access-token")
+def login_access_token() -> Any:
+    return
