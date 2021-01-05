e1ed72e6e060a8b92d06f7bb8e5a3238fc9e0bdf
icefirebear <woungsub1234@gmail.com>
2021-01-05 23:48:10 +0900

    chore: user endpoint 수정

--- src/app/api/api/endpoints/user.py
+++ src/app/api/api/endpoints/user.py
@@ -8,31 +8,31 @@ router = APIRouter()
 
 
 # 유저 정보 가져오기 - Token
-@router.get("")
+@router.get('/')
 def get_user_info() -> Any:
     return
 
 
 # 유저 회원가입
-@router.post("/register")
+@router.post('/register')
 def register_user() -> Any:
     return
 
 
 # 중복 이메일 체크
-@router.post("/emailCheck")
+@router.post('/emailCheck')
 def check_user_email() -> Any:
     return
 
 
 # 유저 정보 수정 - Token
-@router.put("")
+@router.put('/')
 def update_user_info() -> Any:
     return
 
 
 # 유저 탈퇴 - Token
-@router.delete("")
+@router.delete('/')
 def delete_user_account() -> Any:
     return
 
