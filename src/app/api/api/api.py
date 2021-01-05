40f682c68c0d6e16b530c167fb34e920282af09a
icefirebear <woungsub1234@gmail.com>
2021-01-05 22:45:53 +0900

    chore: 라우트 prefix 수정

--- src/app/api/api/api.py
+++ src/app/api/api/api.py
@@ -4,5 +4,5 @@ from .endpoints import oauth, user
 
 api_router = APIRouter()
 
-api_router.include_router(user.router, prefix="user/", tags=["users"])
-api_router.include_router(oauth.router, prefix="oauth/", tags=["oauth"])
+api_router.include_router(user.router, prefix="/user", tags=["users"])
+api_router.include_router(oauth.router, prefix="/oauth", tags=["oauth"])
