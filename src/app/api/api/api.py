75a383dbd0641e35309f347948ea949f5fc8a184
icefirebear <woungsub1234@gmail.com>
2021-01-05 22:44:30 +0900

    chore: user, oauth 라우트 설정

---
+++ src/app/api/api/api.py
@@ -0,0 +1,8 @@
+from fastapi import APIRouter
+
+from .endpoints import oauth, user
+
+api_router = APIRouter()
+
+api_router.include_router(user.router, prefix="user/", tags=["users"])
+api_router.include_router(oauth.router, prefix="oauth/", tags=["oauth"])
