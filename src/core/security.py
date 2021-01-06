659b5f5611340c48e4578c1c696a32b1a9cd17ad
hanbin8269 <hanbin8269@gmail.com>
2021-01-07 00:24:43 +0900

    Feat : 유저 토큰 생성 함수

---
+++ src/core/security.py
@@ -0,0 +1,23 @@
+import jwt
+
+from datetime import datetime, timedelta
+from typing import Optional
+
+from app import config
+
+SECRET_KEY = config.SECRET_KEY
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
