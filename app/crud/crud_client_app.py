8c16eed1552061e2c2d42b4dbcafbf3312ac2c84
hmoon630 <s19017@gsm.hs.kr>
2021-01-11 23:14:19 +0900

    feat: 불필요한 함수 제거

--- app/crud/crud_client_app.py
+++ app/crud/crud_client_app.py
@@ -10,25 +10,5 @@ class CRUDClientApp(CRUDBase[ClientApp, ClientAppCreate, ClientAppUpdate]):
     def get_multi(self, db: Session, *, id):
         return db.query(self.model).filter(self.model.owner_uuid == id).all()
 
-    def create(self, db: Session, *, obj_in: ClientAppCreate):
-        db_obj = ClientApp()
-        db.add(db_obj)
-        db.commit()
-        db.refresh(db_obj)
-        return db_obj
-
-    def update(
-        self,
-        db: Session,
-        *,
-        db_obj: User,
-        obj_in: Union[ClientAppUpdate, Dict[str, Any]]
-    ) -> User:
-        if isinstance(obj_in, dict):
-            update_data = obj_in
-        else:
-            update_data = obj_in.dict(exclude_unset=True)
-        return super().update(db, db_obj=db_obj, obj_in=update_data)
-
 
 client_app = CRUDClientApp(ClientApp)
