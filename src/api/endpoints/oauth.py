823d85cac416fad1aa4455e79df4979fa21af6de
hmoon630 <s19017@gsm.hs.kr>
2021-01-11 17:53:25 +0900

    feat: 클라이언트 앱 endpoint 추가

--- src/api/endpoints/oauth.py
+++ src/api/endpoints/oauth.py
@@ -7,24 +7,34 @@ router = APIRouter()
 
 
 # access_token 받아오기
-@router.get('/authorize')
+@router.get("/authorize")
 def get_access_token() -> Any:
     return
 
 
 # OAuth  목록 가져오기 - Token
-@router.get('/')
+@router.get("/")
 def get_app_list() -> Any:
     return
 
 
+@router.get("/")
+def get_app_info():
+    return
+
+
 # OAuth 클라이언트 생성 - Token
-@router.post('/')
-def create_oauth_client():
+@router.post("/")
+def create_app():
+    return
+
+
+@router.put("/")
+def update_app():
     return
 
 
 # OAuth 클라이언트 삭제
-@router.delete('/')
-def delete_oauth_client():
+@router.delete("/")
+def delete_app():
     return
