e2b5299dd7ab5c9a476cbe2e0fe9007908b73f1c
hanbin8269 <hanbin8269@gmail.com>
2021-01-12 01:41:38 +0900

    chore : change parameter

--- app/crud/base.py
+++ app/crud/base.py
@@ -21,8 +21,8 @@ class CRUDBase(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
         """
         self.model = model
 
-    def get(self, db: Session, id: Any) -> Optional[ModelType]:
-        return db.query(self.model).filter(self.model.id == id).first()
+    def get(self, db: Session, email: Any) -> Optional[ModelType]:
+        return db.query(self.model).filter(self.model.email == email).first()
 
     def get_multi(
         self, db: Session, *, skip: int = 0, limit: int = 100
