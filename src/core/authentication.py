732d7641dfbea3a9773ff275de44e96547e3ea7e
hanbin8269 <hanbin8269@gmail.com>
2021-01-11 14:10:26 +0900

    chore : create_access_token 함수 클래스 메소드로 변경

--- src/core/authentication.py
+++ src/core/authentication.py
@@ -1,23 +1,25 @@
 import jwt
 
 from datetime import datetime, timedelta
-from typing import Optional
+from typing import Optional, Tuple
 
 from app import config
+from model import User
+from .base_authentication import BaseAuthentication
 
 SECRET_KEY = config.SECRET_KEY
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
+class JWTAuthentication(BaseAuthentication):
 
-def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
-    to_encode = data.copy()
-    if expires_delta:
-        expire = datetime.utcnow() + expires_delta
-    else:
-        expire = datetime.utcnow() + timedelta(minutes=15)
-    to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return encoded_jwt
-
+    def authenticate(self, email, password) -> Tuple[User,str]:
+        to_encode = data.copy()
+        if expires_delta:
+            expire = datetime.utcnow() + expires_delta
+        else:
+            expire = datetime.utcnow() + timedelta(minutes=15)
+        to_encode.update({"exp": expire})
+        encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+        return encoded_jwt
 
