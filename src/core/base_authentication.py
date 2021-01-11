2f1c7e61568564e998967050e188198e1e710130
hanbin8269 <hanbin8269@gmail.com>
2021-01-11 14:10:04 +0900

    chore : return type tuple[User, str] 설정

--- src/core/base_authentication.py
+++ src/core/base_authentication.py
@@ -1,10 +1,10 @@
 from datetime import timedelta
 from typing import Optional, Tuple
-
+from model.user import User
 
 class BaseAuthentication():
 
-    def authenticate(self, email, password) -> Tuple[None,str]:
+    def authenticate(self, email, password) -> Tuple[User,str]:
         """
         email과 password의 유효성 검사와 login 처리 (토큰 발급)
         Returns: (User , token)
