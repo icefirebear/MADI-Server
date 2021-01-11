dbb8c4009d99a9027c8c80b84f4bfcb643a26b53
icefirebear <woungsub1234@gmail.com>
2021-01-11 23:16:15 +0900

    feat: check_user_email 기능구현

--- app/api/endpoints/user.py
+++ app/api/endpoints/user.py
@@ -1,9 +1,10 @@
 from typing import Any, Optional
 
-import uuid
+import re
 from fastapi import APIRouter, Depends, HTTPException, Header, Response, Body
 from sqlalchemy.orm import Session
 from fastapi.encoders import jsonable_encoder
+from pydantic import EmailStr
 
 from app.api import dependencies
 from app.core.config import Settings
@@ -30,15 +31,23 @@ def register_user(
     if user:
         raise HTTPException(status_code=400, detail="This email already exists.")
     user = crud.user.create(db, obj_in=user_in)
-    return Response(
-        status_code=201,
-    )
+    return Response(status_code=201)
 
 
 # 중복 이메일 체크
 @router.post("/email-check")
-def check_user_email() -> Any:
-    return
+def check_user_email(
+    db: Session = Depends(dependencies.get_db), email: EmailStr = None
+) -> Any:
+
+    # 이메일 형식검사
+    p = re.compile("^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$")
+    if not p.match(email) != None:
+        raise HTTPException(status_code=422, detail="invalid email format")
+    user = crud.user.get_by_email(db, email=email)
+    if user:
+        raise HTTPException(status_code=400, detail="This email already exists.")
+    return Response(status_code=200)
 
 
 # 유저 정보 수정 - Token
