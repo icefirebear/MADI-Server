861000c601c94fc329e845b7462e4b4d44ef0963
hanbin8269 <hanbin8269@gmail.com>
2021-01-11 14:38:01 +0900

    chore : password hash 함수 생성 및 인증 메서드 매개변수 변경

--- src/core/authentication.py
+++ src/core/authentication.py
@@ -1,19 +1,30 @@
-import jwt
+from jose import jwt
 
 from datetime import datetime, timedelta
 from typing import Optional, Tuple
 
+
+from passlib.context import CryptContext
 from app import config
 from model import User
+from crud import crud_user
 from .base_authentication import BaseAuthentication
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 SECRET_KEY = config.SECRET_KEY
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
+def get_password_hash(password : str) -> str:
+    return pwd_context.hash(password)
+
+def verify_password(plain_password : str, hashed_password:str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
+
 class JWTAuthentication(BaseAuthentication):
 
-    def authenticate(self, email, password) -> Tuple[User,str]:
+    def authenticate(self,data, expires_delta : Optional[datetime]) -> Tuple[User,str]:
         to_encode = data.copy()
         if expires_delta:
             expire = datetime.utcnow() + expires_delta
