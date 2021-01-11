1c5deed933e6ab92db3bae76b7e514074197e080
hanbin8269 <hanbin8269@gmail.com>
2021-01-11 15:42:25 +0900

    chore : reformat file and rename method name

--- src/core/authentication.py
+++ src/core/authentication.py
@@ -3,34 +3,29 @@ from jose import jwt
 from datetime import datetime, timedelta
 from typing import Optional, Tuple
 
-
 from passlib.context import CryptContext
-from app import config
-from model import User
-from crud import crud_user
+from core.config import settings
 from .base_authentication import BaseAuthentication
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-SECRET_KEY = config.SECRET_KEY
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
-def get_password_hash(password : str) -> str:
+def get_password_hash(password: str) -> str:
     return pwd_context.hash(password)
 
-def verify_password(plain_password : str, hashed_password:str) -> bool:
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
+
 
 class JWTAuthentication(BaseAuthentication):
 
-    def authenticate(self,data, expires_delta : Optional[datetime]) -> Tuple[User,str]:
+    def obtain_token(self, data: dict, expires_delta: Optional[timedelta] = None):
         to_encode = data.copy()
         if expires_delta:
             expire = datetime.utcnow() + expires_delta
         else:
             expire = datetime.utcnow() + timedelta(minutes=15)
         to_encode.update({"exp": expire})
-        encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+        encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
         return encoded_jwt
-
