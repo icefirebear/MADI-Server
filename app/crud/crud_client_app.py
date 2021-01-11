0506f7a48ab6c74802ba190dcc493f5bb7e6e525
hmoon630 <s19017@gsm.hs.kr>
2021-01-11 22:53:56 +0900

    feat: 클라이언트 앱 CRUD

--- app/crud/crud_client_app.py
+++ app/crud/crud_client_app.py
@@ -1,3 +1,4 @@
+from typing import Any, Dict, Optional, Union
 from sqlalchemy.orm import Session
 
 from .base import CRUDBase
@@ -7,7 +8,27 @@ from app.schema.client_app import ClientAppCreate, ClientAppUpdate
 
 class CRUDClientApp(CRUDBase[ClientApp, ClientAppCreate, ClientAppUpdate]):
     def get_multi(self, db: Session, *, id):
-        pass
+        return db.query(self.model).filter(self.model.owner_uuid == id).all()
+
+    def create(self, db: Session, *, obj_in: ClientAppCreate):
+        db_obj = ClientApp()
+        db.add(db_obj)
+        db.commit()
+        db.refresh(db_obj)
+        return db_obj
+
+    def update(
+        self,
+        db: Session,
+        *,
+        db_obj: User,
+        obj_in: Union[ClientAppUpdate, Dict[str, Any]]
+    ) -> User:
+        if isinstance(obj_in, dict):
+            update_data = obj_in
+        else:
+            update_data = obj_in.dict(exclude_unset=True)
+        return super().update(db, db_obj=db_obj, obj_in=update_data)
 
 
 client_app = CRUDClientApp(ClientApp)
