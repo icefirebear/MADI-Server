1e4f5de4f54b969912fb949cae7a505ffb1c0f58
hanbin8269 <hanbin8269@gmail.com>
2021-01-12 00:18:19 +0900

    chore : add payload

--- app/api/endpoints/login.py
+++ app/api/endpoints/login.py
@@ -4,7 +4,8 @@ from fastapi import APIRouter, Depends, HTTPException, Body
 from sqlalchemy.orm import Session
 
 from app.core.config import settings
-from app.schema.user import UserLogin, Token
+from app.schema.user import UserLogin
+from app.schema.token import Token
 from app.api import dependencies
 from app.core.security import obtain_token
 from app import crud
@@ -15,22 +16,19 @@ router = APIRouter()
 # 기본정보로 로그인
 @router.post("/", response_model=Token)
 def login_by_general(
-    db: Session = Depends(dependencies.get_db), form_data: UserLogin = Body(...)
+        db: Session = Depends(dependencies.get_db), form_data: UserLogin = Body(...)
 ) -> Any:
-    print(form_data)
     user = crud.user.authenticate(
         db, email=form_data.email, password=form_data.password
     )
     if not user:
         raise HTTPException(status_code=400, detail="Incorrect email or password")
-    elif not crud.user.is_active(user):
-        raise HTTPException(status_code=400, detail="Inactive user")
-    access_token_expires = timedelta(minute=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token_expires = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    payload = {
+        "sub": user.email,
+        "std_no": user.std_no,
+        "name": user.name,
+        "gender": user.gender.value
+    }
 
-    return {"token": obtain_token(user.email, expires_delta=access_token_expires)}
-
-
-# access-token 으로 로그인
-@router.get("/access-token")
-def login_by_access_token() -> Any:
-    return
+    return {"token": obtain_token(payload=payload, expires_delta=access_token_expires)}
