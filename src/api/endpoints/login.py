bca68da9abcdbce265ef501d1e38d15ef6ea6a98
hanbin8269 <hanbin8269@gmail.com>
2021-01-11 15:48:08 +0900

    feat : jwt 인증방식 login api 개발

--- src/api/endpoints/login.py
+++ src/api/endpoints/login.py
@@ -1,14 +1,33 @@
+from datetime import timedelta
 from typing import Any
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
+
+from core.config import settings
+from schema.user import UserLogin
+from api import dependencies
+from core.authentication import JWTAuthentication
+import crud
 
 router = APIRouter()
 
 
 # 기본정보로 로그인
 @router.post('/')
-def login_by_general() -> Any:
-    return
+def login_by_general(db: Session = Depends(dependencies.get_db()), form_data: UserLogin = Depends()) -> Any:
+    user = crud.user.authenticate(db, email=form_data.email, password=form_data.password)
+
+    if not user:
+        raise HTTPException(status_code=400, detail="Incorrect email or password")
+    elif not crud.user.is_active(user):
+        raise HTTPException(status_code=400, detail="Inactive user")
+    access_token_expires = timedelta(minute=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+
+    return {
+        "token": JWTAuthentication.obtain_token(
+            user, expires_delta=access_token_expires
+        )
+    }
 
 
 # access-token 으로 로그인
