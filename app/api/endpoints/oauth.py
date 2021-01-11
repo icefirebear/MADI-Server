94b795a32c82c5ec2eea8f5bef287cb6410461e0
hmoon630 <s19017@gsm.hs.kr>
2021-01-11 22:53:04 +0900

    feat: request data 정의

--- app/api/endpoints/oauth.py
+++ app/api/endpoints/oauth.py
@@ -4,47 +4,74 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from fastapi_sqlalchemy import db
 
-from app import crud
+from app import crud, model
 from app.api import dependencies
+from app.schema.client_app import ClientAppCreate, ClientAppUpdate
 
 router = APIRouter()
 
 
-# access_token 받아오기
+# access_token 발행하기
 @router.get("/authorize")
-async def issue_access_token(db: Session = Depends(dependencies.get_db)) -> Any:
+async def issue_access_token(
+    db: Session = Depends(dependencies.get_db),
+    current_user: model.User = Depends(dependencies.get_current_user),
+) -> Any:
     return
 
 
 @router.get("/access-token")
-async def verify_access_token(db: Session = Depends(dependencies.get_db)) -> Any:
+async def verify_access_token(
+    db: Session = Depends(dependencies.get_db),
+    current_user: model.User = Depends(dependencies.get_current_user),
+) -> Any:
     return
 
 
 # OAuth  목록 가져오기 - Token
 @router.get("/")
-async def get_app_list(db: Session = Depends(dependencies.get_db)) -> Any:
+async def get_app_list(
+    db: Session = Depends(dependencies.get_db),
+    current_user: model.User = Depends(dependencies.get_current_user),
+) -> Any:
     my_apps = crud.client_app.get_multi(db)
     return my_apps
 
 
-@router.get("/")
-async def get_app_info(db: Session = Depends(dependencies.get_db)):
+@router.get("/{app_id}")
+async def get_app_info(
+    app_id: str,
+    db: Session = Depends(dependencies.get_db),
+    current_user: model.User = Depends(dependencies.get_current_user),
+):
     return
 
 
 # OAuth 클라이언트 생성 - Token
 @router.post("/")
-async def create_app(db: Session = Depends(dependencies.get_db)):
+async def create_app(
+    user_in: ClientAppCreate,
+    db: Session = Depends(dependencies.get_db),
+    current_user: model.User = Depends(dependencies.get_current_user),
+):
     return
 
 
-@router.put("/")
-async def update_app(db: Session = Depends(dependencies.get_db)):
+@router.put("/{app_id}")
+async def update_app(
+    app_id: str,
+    user_in: ClientAppUpdate,
+    db: Session = Depends(dependencies.get_db),
+    current_user: model.User = Depends(dependencies.get_current_user),
+):
     return
 
 
 # OAuth 클라이언트 삭제
-@router.delete("/")
-async def delete_app(db: Session = Depends(dependencies.get_db)):
+@router.delete("/{app_id}")
+async def delete_app(
+    app_id: str,
+    db: Session = Depends(dependencies.get_db),
+    current_user: model.User = Depends(dependencies.get_current_user),
+):
     return
