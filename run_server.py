43398508c8ec3696348b2f536b9dea30ef37a1a7
hanbin8269 <hanbin8269@gmail.com>
2021-01-12 19:30:34 +0900

    chore : 불필요한 예외처리 제거 및 설정을 core.config.settings로 부터 불러옴

--- run_server.py
+++ run_server.py
@@ -1,18 +1,16 @@
 import uvicorn
-
 from configparser import ConfigParser
+from app.core.config import settings
 
 
 def main():
     config = ConfigParser()
     config.read("./config.ini")
 
-    try:
-        PORT: int = int(config.get("default", "PORT"))
-    except Exception as e:
-        PORT: int = 5000
+    PORT = settings.SERVER_PORT
+    HOST = settings.SERVER_HOST
 
-    uvicorn.run("app.main:app", port=PORT, reload=True)
+    uvicorn.run("app.main:app", host=HOST, port=PORT, reload=True)
 
 
 if __name__ == "__main__":
