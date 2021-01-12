fcc5bd1928b4c3221a32673a11b0178c5e05ea85
hmoon630 <s19017@gsm.hs.kr>
2021-01-12 22:49:37 +0900

    feat: 클라이언트 앱 생성 API 완성

--- app/crud/crud_client_app.py
+++ app/crud/crud_client_app.py
@@ -3,31 +3,69 @@ from sqlalchemy.orm import Session
 import random, string
 
 from .base import CRUDBase
-from app import crud
+from app import crud, model
 from app.model.client_app import ClientApp
-from app.schema.client_app import ClientAppCreate, ClientAppUpdate
+from app.schema.client_app import ClientAppCreate, ClientAppUpdate, ClientAppInDB
 
 
 class CRUDClientApp(CRUDBase[ClientApp, ClientAppCreate, ClientAppUpdate]):
-    def check_duplicate_id(self, db: Session, id):
-        return db.query(self.model).filter(self.model.id == id).first()
+    def check_duplicate_id(self, db: Session, app_id):
+        return db.query(self.model).filter(self.model.app_id == app_id).first()
+
+    def get(self, db: Session, *, app_id):
+        app = db.query(self.model).filter(self.model.app_id == app_id).first()
+        app.authority = [
+            item.authority.value
+            for item in (
+                db.query(model.Authority).filter(model.Authority.app_id == app_id).all()
+            )
+        ]
+        app.approved_domain = [
+            item.domain
+            for item in (
+                db.query(model.ApprovedDomain)
+                .filter(model.ApprovedDomain.app_id == app_id)
+                .all()
+            )
+        ]
+
+        return app
 
     def get_multi(self, db: Session, *, owner_uuid):
         return db.query(self.model).filter(self.model.owner_uuid == owner_uuid).all()
 
-    def create(self, db, *, obj_in):
+    def create(self, db: Session, *, obj_in: ClientAppCreate):
         random_id = "".join(random.choices(string.ascii_letters + string.digits, k=9))
         while self.check_duplicate_id(db, random_id):
             random_id = "".join(
                 random.choices(string.ascii_letters + string.digits, k=9)
             )
+        obj_in.app_id = random_id
+        obj_in.secret_key = "".join(
+            random.choices(string.ascii_letters + string.digits, k=20)
+        )
 
-        obj_in.id = random_id
-        obj_in.secret_key = random.choices(string.ascii_letters + string.digits, k=20)
-        app = super().create(db, obj_in=obj_in)
-        crud.authority.create_multi(app.id, obj_in.authority)
+        app = ClientApp(
+            app_id=obj_in.app_id,
+            owner_uuid=obj_in.owner_uuid,
+            secret_key=obj_in.secret_key,
+            name=obj_in.name,
+            image=str(obj_in.image),
+            redirect_uri=str(obj_in.redirect_uri),
+        )
 
-        return app
+        db.add(app)
+        db.commit()
+        db.refresh(app)
+
+        crud.authority.create_multi(
+            db, app_id=obj_in.app_id, authorities=obj_in.authority
+        )
+        crud.approved_domain.create_multi(
+            db, app_id=obj_in.app_id, domains=obj_in.approved_domain
+        )
+
+        return obj_in
 
 
 client_app = CRUDClientApp(ClientApp)
